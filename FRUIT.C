6a4115438c33820c0b8082bfe1f29266d2e2b2af
Arya Maddel <maddelarya@gmail.com>
2022-05-01 20:24:25 +0530

    formatted

--- FRUIT.C
+++ FRUIT.C
@@ -1,18 +1,18 @@
-//Fruit Shop Management Program in C by Ved Asawa, Avneesh Jagtap and Arya Maddel
-#include <stdio.h>	//Include Header files
+// Fruit Shop Management Program in C by Ved Asawa, Avneesh Jagtap and Arya Maddel
+#include <stdio.h> //Include Header files
 #include <conio.h>
-struct fruit		//Fruit Structure
+struct fruit // Fruit Structure
 {
 	int code;
 	int quantity;
-}fr[5];
+} fr[5];
 
 void main()
 {
-int i, n, h, a;		//Variable Declaration
-float f1, f2, f3, f4 ,f5;
-clrscr();
-	printf("\t\t\tWelcome to our Fruit Shop");		//User Interface
+	int i, n, h, a; // Variable Declaration
+	float f1, f2, f3, f4, f5;
+	clrscr();
+	printf("\t\t\tWelcome to our Fruit Shop"); // User Interface
 	printf("\n\n\nToday in our shop we are selling : ");
 
 	printf("\n\n-----------------------------------------------------");
@@ -28,47 +28,47 @@ clrscr();
 	printf("\n\nHow many fruits you have to order : ");
 	scanf("%d", &n);
 
-	for (i = 0;i < n; i++)					//Condition
+	for (i = 0; i < n; i++) // Condition
 	{
 		printf("\nEnter Item Number : ");
 		scanf("%d", &a);
 
-	if (a==1)
-	{
-		printf("Number of Banana = ");
-		scanf("%d",&fr[1].quantity);
-		f1=fr[1].quantity;
+		if (a == 1)
+		{
+			printf("Number of Banana = ");
+			scanf("%d", &fr[1].quantity);
+			f1 = fr[1].quantity;
+		}
+		else if (a == 2)
+		{
+			printf("Grams of Oranges = ");
+			scanf("%d", &fr[2].quantity);
+			f2 = fr[2].quantity;
+		}
+		else if (a == 3)
+		{
+			printf("Grams of Apple = ");
+			scanf("%d", &fr[3].quantity);
+			f3 = fr[3].quantity;
+		}
+		else if (a == 4)
+		{
+			printf("Grams of Watermelon = ");
+			scanf("%d", &fr[4].quantity);
+			f4 = fr[4].quantity;
+		}
+		else if (a == 5)
+		{
+			printf("Grams of Guava = ");
+			scanf("%d", &fr[5].quantity);
+			f5 = fr[5].quantity;
+		}
+		else
+		{
+			printf("Product Invalid");
+		}
 	}
-	else if (a==2)
-	{
-		printf("Grams of Oranges = ");
-		scanf("%d",&fr[2].quantity);
-		f2=fr[2].quantity;
-	}
-	else if (a==3)
-	{
-		printf("Grams of Apple = ");
-		scanf("%d",&fr[3].quantity);
-		f3=fr[3].quantity;
-	}
-	else if (a==4)
-	{
-		printf("Grams of Watermelon = ");
-		scanf("%d",&fr[4].quantity);
-		f4=fr[4].quantity;
-	}
-	else if (a==5)
-	{
-		printf("Grams of Guava = ");
-		scanf("%d",&fr[5].quantity);
-		f5=fr[5].quantity;
-	}
-	else
-	{
-		printf("Product Invalid");
-	}
-	}
-	printf("\n-------------------------------------------");		//Final Bill
+	printf("\n-------------------------------------------"); // Final Bill
 	clrscr();
 	printf("\n-----------------------------------------------------------------");
 	printf("\nYour Reciept\n");
@@ -81,7 +81,7 @@ clrscr();
 	printf("\n|\t4.\t|\tWatermelon |\t%.2f\t|\t%.2f\t|", f4, f4 * 0.02);
 	printf("\n|\t5.\t|\tGuava      |\t%.2f\t|\t%.2f\t|", f5, f5 * 0.06);
 	printf("\n-----------------------------------------------------------------");
-	printf("\t\tTotal Amount = Rs.%.2f\n", ((f1*3.33)+(f2*0.1)+(f3*0.2)+(f4*0.02)+(f5*0.06)));
+	printf("\t\tTotal Amount = Rs.%.2f\n", ((f1 * 3.33) + (f2 * 0.1) + (f3 * 0.2) + (f4 * 0.02) + (f5 * 0.06)));
 	printf("\nThanks you for shopping with us!");
-getch();
+	getch();
 }
